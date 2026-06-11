3c89c82f0789fb0c6af42be116cc63702dfce72c
Tapiwa Kundishora <tapiwakundi@gmail.com>
2026-06-10 22:26:34 -0600

    Initial Commit

---
+++ mywallpaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mywallpaper
+//
+//  Created by Tapiwa Kundishora on 2026-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mywallpaper/mywallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  mywallpaperApp.swift
+//  mywallpaper
+//
+//  Created by Tapiwa Kundishora on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct mywallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
